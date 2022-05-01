4c181ddf8afd9e212291e789016e4a7c1d9b51c8
Abdus Samiul Basir <samiulprangon2013@gmail.com>
2022-05-01 17:00:05 +0600

    Delete comment in index.html and create 4 components header, part, content, total and use them in app component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+const Hearder = ({course}) => <p>{course}</p>;
 
-function App() {
+const Part = ({part, exercise}) => <p>{part} {exercise}</p>;
+
+const Content = ({parts}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+        <Part part={parts[0].name}/>
+        <Part part={parts[1].name}/>
+        <Part part={parts[2].name}/>
+    </>
+  );
+};
+
+
+const Total = ({num}) => {
+  let total = 0;
+  num.forEach(element => {total += element.exercises});
+  return (<p>The total is {total}.</p>);
+};
+
+  
+const App = () => {
+  const course = {
+    name: "Half Stack application development.",
+    parts: [
+      {
+        name: "Fundamentals of React.",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data.",
+        exercises: 7,
+      },
+      {
+        name: "State of a component.",
+        exercises: 14,
+      },
+    ],
+  };
+  return (
+    <div>
+      <Hearder course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total num={course.parts}/>
     </div>
   );
-}
-
+};
+  
 export default App;
